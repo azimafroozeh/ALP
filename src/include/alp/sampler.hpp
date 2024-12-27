61ee7f8378c1be93295ec71378fb0d06d117cdd0
azimafroozeh <afroozeh3@gmail.com>
2024-12-27 02:19:14 +0100

    [try] even simpler

--- src/include/alp/sampler.hpp
+++ src/include/alp/sampler.hpp
@@ -14,6 +14,7 @@ uint64_t first_level_sample(const PT* data_p, const uint64_t n_values, PT* sampl
 	uint64_t       data_idx      = 0;
 
 	for (uint64_t vector_idx = 0; vector_idx < n_vectors; vector_idx++) {
+		constexpr uint64_t n_sampled_increments = 32;
 		//! We sample equidistant vectors; to do this we skip a fixed values of vectors
 		//! If we are not in the correct jump, we do not take sample from this vector
 		if (const bool must_select_rowgroup_sample = (vector_idx % config::ROWGROUP_SAMPLES_JUMP) == 0;
@@ -21,9 +22,6 @@ uint64_t first_level_sample(const PT* data_p, const uint64_t n_values, PT* sampl
 			data_idx += config::VECTOR_SIZE;
 			continue;
 		}
-
-		const uint64_t n_sampled_increments = std::max<uint64_t>(
-		    1, static_cast<uint64_t>(std::ceil(static_cast<double>(config::VECTOR_SIZE) / config::SAMPLES_PER_VECTOR)));
 
 		// Storing the sample of that vector
 		for (uint64_t i = 0; i < config::VECTOR_SIZE; i += n_sampled_increments) {
